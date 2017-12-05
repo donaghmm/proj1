ed9d8d1a378a6959dd28616ffd09c49eab0d670b
mrunalhtrivedi <mrunalhtrivedi@gmail.com>
2017-12-05 07:03:24 +0000

    Apache, Tomcat configuration

---
+++ ansible_apache_tomcat_playbook/bashrc_variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/default-java
+export CATALINA_HOME=/usr/local/tomcat/apache-tomcat-7.0.78/bin
+export TOMCAT_HOME=/usr/local/tomcat/apache-tomcat-7.0.78
+echo "export JAVA_HOME=/usr/lib/jvm/default-java" >> ~/.bashrc
+echo "export CATALINA_HOME=/usr/local/tomcat/apache-tomcat-7.0.78/bin" >> ~/.bashrc
+echo "export TOMCAT_HOME=/usr/local/tomcat/apache-tomcat-7.0.78" >> ~/.bashrc
